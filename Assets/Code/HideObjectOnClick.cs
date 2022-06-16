5ae743a69429166af99b5103e98843a01897362a
Dmitriy Nazarovskiy <dmitriy.nazarovskiy@litslink.com>
2022-06-16 15:20:46 +0300

    Removed useless script HideObjectOnClick.cs

--- Assets/Code/HideObjectOnClick.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-//To be removed
-public class HideObjectOnClick : MonoBehaviour
-{
-	[SerializeField] private GameObject _objectToHide;
-
-	public void HideObject() => _objectToHide.SetActive(false);
-}
