1a7615508d07c7724dae2cfa3f635bd92c0c8fdc
MaksymBezuglyi <88711988+MaksymBezuglyi@users.noreply.github.com>
2022-06-28 12:51:07 +0300

    Cleaning

--- Assets/Scripts/Screens/PasswordPopUp.cs
+++ Assets/Scripts/Screens/PasswordPopUp.cs
@@ -1,23 +1,28 @@
 using System;
-using Core;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace Screens
 {
+	public enum PasswordType : byte
+	{
+		Admin,
+		SuperAdmin
+	}
+	
 	public class PasswordPopUp : MonoBehaviour
 	{
+		private const string ADMIN_PASSWORD_TEXT = "Please enter the administrator password:";
+		private const string SUPER_ADMIN_PASSWORD_TEXT = "Please enter the super administrator password:";
 		[SerializeField] private Button _okButton, _cancelButton;
 		[SerializeField] private InputField _inputField;
 		[SerializeField] private Text _descriptionText;
-
-		private const string AdminPassText = "Please enter the administrator password:";
-		private const string SuperPassText = "Please enter the super administrator password:";
 		
 		public void Init(Action<string> onOkButtonClick, PasswordType type)
 		{
 			_cancelButton.onClick.AddListener(Close);
 			_okButton.onClick.AddListener(() => onOkButtonClick.Invoke(_inputField.text));
+			
 			SetTextByType(type);
 		}
 
@@ -26,10 +31,10 @@ namespace Screens
 			switch (type)
 			{
 				case PasswordType.Admin:
-					_descriptionText.text = AdminPassText;
+					_descriptionText.text = ADMIN_PASSWORD_TEXT;
 					break;
 				case PasswordType.SuperAdmin:
-					_descriptionText.text = SuperPassText;
+					_descriptionText.text = SUPER_ADMIN_PASSWORD_TEXT;
 					break;
 				default:
 					throw new ArgumentOutOfRangeException(nameof(type), type, null);
@@ -37,52 +42,5 @@ namespace Screens
 		}
 		
 		private void Close() => Destroy(gameObject);
-	}
-
-	public enum PasswordType
-	{
-		Admin,
-		SuperAdmin
-	}
-
-	public static class LogIn
-	{
-		private static bool _adminIsLogIn = false;
-		private static bool _superAdminIsLogIn = false;
-		
-		public static void LogInByType(PasswordType type)
-		{
-			switch (type)
-			{
-				case PasswordType.Admin:
-					_adminIsLogIn = true;
-					break;
-				case PasswordType.SuperAdmin:
-					_superAdminIsLogIn = true;
-					break;
-				default:
-					throw new ArgumentOutOfRangeException(nameof(type), type, null);
-			}
-		}
-
-		public static bool CheckIsLogInByType(PasswordType type)
-		{
-			return type switch
-			{
-				PasswordType.Admin => _adminIsLogIn,
-				PasswordType.SuperAdmin => _superAdminIsLogIn,
-				_ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
-			};
-		}
-
-		public static string GetPasswordByType(PasswordType type)
-		{
-			return type switch
-			{
-				PasswordType.Admin => Constants.CorrectAdminPass,
-				PasswordType.SuperAdmin => Constants.CorrectSuperPass,
-				_ => null
-			};
-		}
 	}
 }
