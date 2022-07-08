66f955ea13726b53a2a557089bc0f70757fd2a06
MaksymBezuglyi <88711988+MaksymBezuglyi@users.noreply.github.com>
2022-07-08 18:28:50 +0300

    [EN-44] add settings class and all Dependency that we need in this window

--- Assets/Scripts/Screens/OptionsMenu.cs
+++ Assets/Scripts/Screens/OptionsMenu.cs
@@ -1,3 +1,5 @@
+using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using Object = UnityEngine.Object;
@@ -7,10 +9,47 @@ namespace Screens
 	public class OptionsMenu : MonoBehaviour
 	{
 		[SerializeField] private Button _cancelButton;
-
+		[SerializeField] private Toggle _sound;
+		[SerializeField] private Toggle _rotation;
+		[SerializeField] private TMP_Dropdown _outputsNumber;
+		[SerializeField] private TMP_InputField _cuoCoreIp;
+		[SerializeField] private TMP_InputField _cuoCorePort;
+		
 		public void Init()
 		{
-			_cancelButton.onClick.AddListener(() => Destroy(gameObject));
+			SetDefaultValue();
+			
+			_cancelButton.onClick.AddListener(() =>
+			{
+				OptionsSettings.Save(_sound,_rotation, _outputsNumber.value, _cuoCoreIp.text, Convert.ToInt32(_cuoCorePort.text));
+				Destroy(gameObject);
+			});
+		}
+
+		private void SetDefaultValue()
+		{
+			_sound.isOn = OptionsSettings.Sound;
+			_rotation.isOn = OptionsSettings.Rotation;
+			_outputsNumber.value = OptionsSettings.NumberOfOutputs;
+			_cuoCoreIp.text = OptionsSettings.CuoCoreIp;
+			_cuoCorePort.text = OptionsSettings.CuoCorePort.ToString();
+		}
+	}
+
+	public static class OptionsSettings
+	{
+		public static bool Sound, Rotation;
+		public static int NumberOfOutputs;
+		public static string CuoCoreIp = "192.168.1.10";
+		public static int CuoCorePort = 7000;
+
+		public static void Save(bool sound, bool rotation, int outputsNumber, string cuoCoreIp, int cuoCorePort)
+		{
+			Sound = sound;
+			Rotation = rotation;
+			NumberOfOutputs = outputsNumber;
+			CuoCoreIp = cuoCoreIp;
+			CuoCorePort = cuoCorePort;
 		}
 	}
 }
