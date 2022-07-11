5dc06f87819bcc2c350717ae8b270e27a442342e
Nazarivskyi Dmytro <dmitriy.nazarovskiy@litslink.com>
2022-07-11 13:13:30 +0300

    EN-8 Moved some code to the proper place

--- Assets/Scripts/Core/UIHelper.cs
+++ Assets/Scripts/Core/UIHelper.cs
@@ -5,14 +5,15 @@ namespace Core
 	public static class UIHelper
 	{
 		private const float HalfFactor = 0.5f;
+		public static readonly Vector2 saveWindowSize = new(Settings.ScreenW * HalfFactor, Settings.ScreenH * HalfFactor);
 
 		public static Rect WindowPosition;
 
 		public static void ResetWindowPosition()
 		{
-			WindowPosition = new Rect(Settings.ScreenW * HalfFactor - Settings.saveWindowSize.x * HalfFactor,
-				Screen.height * HalfFactor - Settings.saveWindowSize.y * HalfFactor,
-				Settings.saveWindowSize.x, Settings.saveWindowSize.y);
+			WindowPosition = new Rect(Settings.ScreenW * HalfFactor - saveWindowSize.x * HalfFactor,
+				Screen.height * HalfFactor - saveWindowSize.y * HalfFactor,
+				saveWindowSize.x, saveWindowSize.y);
 		}
 	}
 }
