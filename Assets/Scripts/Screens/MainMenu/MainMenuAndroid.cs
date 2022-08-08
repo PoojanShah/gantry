84a1b6123ebbf3bda06bc96727b263a24e4771eb
Dmitriy Nazarovskiy <dmitriy.nazarovskiy@litslink.com>
2022-08-08 15:30:19 +0300

    EN-124 Started working on new UI and landscape Android resolution.

---
+++ Assets/Scripts/Screens/MainMenu/MainMenuAndroid.cs
@@ -0,0 +1,121 @@
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+using Core;
+using Network;
+using TMPro;
+using UnityEngine.UI;
+using VideoPlaying;
+
+namespace Screens
+{
+	public class MainMenuAndroid : MonoBehaviour
+	{
+		private readonly Vector2Int _ipValueRange = new(2, 255);
+
+		[SerializeField] private Button _exitButton, _connectButton;
+		[SerializeField] private Transform _parent;
+		[SerializeField] private TMP_InputField _ipEnd;
+		[SerializeField] private TMP_Text _ipStart;
+
+		private List<MediaItem> _mediaItems;
+		private ICommonFactory _factory;
+		private GameObject _mediaPrefab;
+
+		public void Init(Action onQuitAction, GameObject mediaPrefab, ICommonFactory factory)
+		{
+			_factory = factory;
+			_mediaPrefab = mediaPrefab;
+
+			_exitButton?.onClick.AddListener(() => { onQuitAction?.Invoke(); });
+			_connectButton.onClick.AddListener(ConnectClicked);
+
+			_ipEnd.onEndEdit.AddListener(VerifyIpNumber);
+
+			InitIpLabel();
+
+			LocalNetworkClient.OnMediaAmountReceived += InitMediaItems;
+		}
+
+		private void ConnectClicked()
+		{
+			if (!int.TryParse(_ipEnd.text, out var number))
+				return;
+
+			LocalNetworkClient.SendPlayMessage(number, -1);
+		}
+
+		private void SendPlayVideoCommand(int videoId)
+		{
+			if (!int.TryParse(_ipEnd.text, out var number)) 
+				return;
+
+			LocalNetworkClient.SendPlayMessage(number, videoId);
+		}
+
+		private void InitIpLabel() => _ipStart.text = NetworkHelper.GetMyIpWithoutLastNumberString();
+
+		private void VerifyIpNumber(string currentValue)
+		{
+			if (int.TryParse(currentValue, out var number))
+			{
+				if (number < _ipValueRange.x)
+					number = _ipValueRange.x;
+				else if (number > _ipValueRange.y)
+					number = _ipValueRange.y;
+
+				_ipEnd.text = number.ToString();
+			}
+		}
+
+		public void SetMediaInteractable()
+		{
+			if(_mediaItems == null || _mediaItems.Count < 1)
+				return;
+
+			foreach (var mediaItem in _mediaItems)
+				mediaItem.SetInteractable(true);
+		}
+
+		private void OnDestroy()
+		{
+			_exitButton?.onClick.RemoveAllListeners();
+			_connectButton.onClick.RemoveAllListeners();
+			_ipEnd.onEndEdit.RemoveAllListeners();
+
+			LocalNetworkClient.OnMediaAmountReceived -= InitMediaItems;
+
+			ClearMediaItems();
+		}
+
+		public void ClearMediaItems()
+		{
+			if(_mediaItems == null || _mediaItems.Count == 0)
+				return;
+
+			foreach (var mediaItem in _mediaItems)
+				Destroy(mediaItem.gameObject);
+
+			_mediaItems.Clear();
+		}
+
+		public void InitMediaItems(int mediaAmount)
+		{
+#if UNITY_ANDROID
+			_connectButton.interactable = false;
+
+			_mediaItems = new List<MediaItem>();
+
+			for (var i = 0; i < mediaAmount; i++)
+			{
+				var mediaItem = _factory.InstantiateObject<MediaItem>(_mediaPrefab, _parent);
+				mediaItem.Init(i, SendPlayVideoCommand);
+
+				_mediaItems.Add(mediaItem);
+			}
+
+			SetMediaInteractable();
+#endif
+		}
+	}
+}
---
+++ Assets/Scripts/Screens/MainMenu/MediaContentController.cs
@@ -0,0 +1,100 @@
+using System.Collections.Generic;
+using Core;
+using Media;
+using UnityEngine;
+using UnityEngine.UI;
+using VideoPlaying;
+
+namespace Assets.Scripts.Screens
+{
+	public class MediaContentController : MonoBehaviour
+	{
+		private const int MEDIA_PER_PAGE = 6;
+
+		[SerializeField] private Button _back, _forward;
+		[SerializeField] private Transform _mediaParent;
+
+		private int _currentPage;
+		private MediaContent[] _media;
+		private ICommonFactory _factory;
+		private GameObject _mediaPrefab;
+		private MediaItem[] _mediaItems;
+
+		public void Init(MediaContent[] media, ICommonFactory factory, GameObject mediaPrefab)
+		{
+			_mediaPrefab = mediaPrefab;
+			_factory = factory;
+			_media = media;
+
+			InitMediaItems();
+
+			_back.onClick.AddListener(ShowPreviousPage);
+			_forward.onClick.AddListener(ShowNextPage);
+
+			_currentPage = 0;
+
+			SetButtonInteractable(true, false);
+			SetButtonInteractable(false, _media.Length > MEDIA_PER_PAGE);
+		}
+
+		private void InitMediaItems()
+		{
+			_mediaItems = new MediaItem[MEDIA_PER_PAGE];
+
+			for (var i = 0; i < MEDIA_PER_PAGE; i++)
+			{
+				var item = _factory.InstantiateObject<MediaItem>(_mediaPrefab, _mediaParent);
+				item.gameObject.SetActive(false);
+
+				_mediaItems[i] = item;
+			}
+		}
+
+		private void DisplayMedia()
+		{
+			var itemsToShow = GetMediaToShow();
+
+			for(var i = 0; i < )
+		}
+
+		public void SetButtonInteractable(bool isBackButton, bool isInteractable)
+		{
+			if(isBackButton)
+				_back.interactable = isInteractable;
+			else 
+				_forward.interactable = isInteractable;
+		}
+
+		private void ShowNextPage() => ChangePage(true);
+		private void ShowPreviousPage() => ChangePage(false);
+
+		private MediaContent[] GetMediaToShow()
+		{
+			var startIndex = (_currentPage + 1) * MEDIA_PER_PAGE;
+			var result = new List<MediaContent>();
+
+			for(var i = startIndex; i < _media.Length; i++)
+				result.Add(_media[i]);
+
+			return result.ToArray();
+		}
+
+		private void ChangePage(bool isPageIncreased)
+		{
+			var maxPageNumber = _media.Length / MEDIA_PER_PAGE;
+
+			Debug.Log(maxPageNumber);
+
+			if (isPageIncreased)
+				_currentPage++;
+			else
+				_currentPage--;
+
+			_currentPage = Mathf.Clamp(_currentPage, 0, maxPageNumber);
+
+			Debug.Log(_currentPage);
+
+			DisplayMedia();
+		}
+	}
+}
